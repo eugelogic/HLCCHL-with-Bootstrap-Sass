e67a44da0419eafde36a1504339a4fc6fb01ba25
Eugene Molari <eugelogic@gmail.com>
2017-06-15 12:01:43 +0100

    Edit Search template page

--- themes/hlcchl/search.php
+++ themes/hlcchl/search.php
@@ -9,40 +9,67 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<!-- SEARCH RESULTS INDEX PAGE
+================================================== -->
+<section id="search-tml">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
 
-		<?php
-		if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'hlcchl' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h2 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'hlcchl' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+				<div class="row">
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+		<!-- Start the Loop -->
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			endwhile;
+		 <!-- SEARCH RESULTS GRID
+		 ================================================== -->
+		 <div class="col-md-6">
+				<div class="news-wrapper">
 
-			the_posts_navigation();
+				 <?php if ( has_post_thumbnail() ) : ?>
+						 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+								 <?php the_post_thumbnail(); ?>
+						 </a>
+				 <?php endif; ?>
 
-		else :
+				 <div class="news-meta">
+					 <?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
+					 <p class="small"><?php the_date(); ?> | Categories: <?php the_category( ', ' ); ?></p>
+					 <div class="news-separator"></div>
+					 <?php the_excerpt(); ?>
+					 <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+				 </div><!-- news-meta -->
+			 </div><!-- news-wrapper -->
+		 </div><!-- col -->
 
-			get_template_part( 'template-parts/content', 'none' );
+				<?php endwhile;
 
-		endif; ?>
+			else :
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+				get_template_part( 'template-parts/content', 'none' );
+
+				endif;  ?>
+
+					</div><!-- row -->
+			</div><!-- col -->
+
+			<!-- SIDEBAR
+			================================================== -->
+			<div class="row">
+				<aside class="col-sm-4">
+					<?php get_sidebar();?>
+				</aside>
+			</div><!-- row -->
+
+		</div><!-- row -->
+	</div><!-- container -->
+</section>
 
 <?php
-get_sidebar();
 get_footer();
