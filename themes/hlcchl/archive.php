954910470b91d7f9ae31c851ccf594eae59a6b6a
Eugene Molari <eugelogic@gmail.com>
2017-06-29 11:53:23 +0100

    Comply to WP coding standards on archive.php

--- themes/hlcchl/archive.php
+++ themes/hlcchl/archive.php
@@ -11,66 +11,66 @@
 
  <!-- NEWS INDEX PAGE
  ================================================== -->
- <?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
  <section id="archive-tml">
- 	<div class="container">
- 		<div class="row">
- 			<div class="col-md-8">
+   <div class="container">
+	 <div class="row">
+	 <div class="col-md-8">
 
 				<header class="page-header">
-					<?php
-						the_archive_title( '<h2 class="page-title">', '</h2>' );
-						the_archive_description( '<div class="archive-description">', '</div>' );
-					?>
+		<?php
+		the_archive_title( '<h2 class="page-title">', '</h2>' );
+		the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
 				</header><!-- .page-header -->
 
- 				<div class="row">
+		<div class="row">
 
- 		<!-- Start the Loop -->
- 		<?php while ( have_posts() ) : the_post(); ?>
+		<!-- Start the Loop -->
+		<?php while ( have_posts() ) : the_post(); ?>
 
- 		 <!-- NEWS INDEX GRID
- 		 ================================================== -->
- 		 <div class="col-md-6">
- 				<div class="post-wrapper">
+	  <!-- NEWS INDEX GRID
+	  ================================================== -->
+	  <div class="col-md-6">
+		<div class="post-wrapper">
 
- 				 <?php if ( has_post_thumbnail() ) : ?>
- 						 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
- 								 <?php the_post_thumbnail(); ?>
- 						 </a>
- 				 <?php endif; ?>
+			<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+				<?php the_post_thumbnail(); ?>
+			</a>
+			<?php endif; ?>
 
- 				 <div class="post-meta">
- 					 <?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
- 					 <p class="small"><?php the_date(); ?> | Categories: <?php the_category( ', ' ); ?></p>
- 					 <div class="post-separator"></div>
- 					 <?php the_excerpt(); ?>
- 					 <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
- 				 </div><!-- post-meta -->
- 			 </div><!-- post-wrapper -->
- 		 </div><!-- col -->
+				  <div class="post-meta">
+		<?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
+					  <p class="small"><?php the_date(); ?> | Categories: <?php the_category( ', ' ); ?></p>
+					  <div class="post-separator"></div>
+		<?php the_excerpt(); ?>
+					  <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+				  </div><!-- post-meta -->
+			  </div><!-- post-wrapper -->
+		  </div><!-- col -->
 
- 				<?php endwhile;
+		<?php endwhile;
 
-        the_posts_navigation();
+		the_posts_navigation();
 
- 				endif;  ?>
+	endif;  ?>
 
- 					</div><!-- row -->
- 			</div><!-- col -->
+	</div><!-- row -->
+	</div><!-- col -->
 
- 			<!-- SIDEBAR
- 			================================================== -->
- 			<div class="row">
- 				<aside class="col-sm-4">
- 					<?php get_sidebar();?>
- 				</aside>
- 			</div><!-- row -->
+			 <!-- SIDEBAR
+			 ================================================== -->
+			 <div class="row">
+				 <aside class="col-sm-4">
+		<?php get_sidebar();?>
+				 </aside>
+			 </div><!-- row -->
 
- 		</div><!-- row -->
- 	</div><!-- container -->
+		 </div><!-- row -->
+	 </div><!-- container -->
  </section>
 
- <?php
- get_footer();
+	<?php
+	get_footer();
