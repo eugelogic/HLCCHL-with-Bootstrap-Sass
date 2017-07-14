10c575e4f2e20f6cf4cb2feeffca9655cb8d7bbb
Eugene Molari <eugelogic@gmail.com>
2017-07-14 16:41:43 +0100

    Comply to WP coding standards on content-none.php

--- themes/hlcchl/template-parts/content-none.php
+++ themes/hlcchl/template-parts/content-none.php
@@ -19,9 +19,14 @@
 
 				<div class="page-content">
 					<?php
-					if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+					if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-						<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'hlcchl' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+						/* translators: 1: link to WP admin new post page. */ ?>
+						<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'hlcchl' ), array(
+							'a' => array(
+							'href' => array(),
+							),
+						) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 					<?php elseif ( is_search() ) : ?>
 
